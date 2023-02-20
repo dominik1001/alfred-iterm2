c3bb02ccf5e7c6fe83e68de0a532b8d93441dc66
dgrusemann <dominik.grusemann@gmail.com>
2023-02-20 12:19:52 +0100

    read every single line

--- iterm.sh
+++ iterm.sh
@@ -9,15 +9,14 @@ query="$1"
 cd $HOME/Library/ApplicationSupport/iTerm2/Scripts/alfred2/alfred2/
 x=$(python3 alfred2.py | grep -i "$query")
 
-
 items=""
-for app in $x; do
+while IFS= read -r app; do
   item="{\"uid\":\""$app"\", \"title\":\""$app"\", \"arg\":\""$app"\", \"valid\": true}"
   if [[ $items == "" ]]; then
     items="$item"
   else
     items="$items,$item"
   fi
-done
+done <<<"$x"
 
 echo -e "{ \"items\": [$items]}"
